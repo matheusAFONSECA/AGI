8b574368508a57280cf50fd9ec24202fea31e436
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-06-23 15:55:43 -0300

    Exercicio 2 OK

---
+++ Exercicio2.py
@@ -0,0 +1,36 @@
+# imporatções necessarias para solucionar a questão
+from sympy import Derivative, Symbol
+
+# definindo o componente C da expressão
+c = 1994 % 10       # C -> resto do numero de metricula dividido por 10
+
+# definindo o simbolo
+t = Symbol('t')
+
+# definindo a expressão de deslocamento do objeto
+def funcao(t):
+    return (t**(1/3)/5) + ((c+1)/(t**3)) - ((c+2)*(t**2)) - 15
+
+
+# Calculando a derivada da função do deslocamento -> velocidade
+Derivada = Derivative(funcao(t), t).doit()
+
+# Mostrando a equação de velocidade
+print("Equação da velocidade: " + str(Derivada))
+
+# Calculando a velocidade aos 7s
+Velocidade = Derivative(funcao(t), t).doit().subs({t:7})
+print("Equação da velocidade em 7s : " + str(Velocidade))
+
+# Calculando a derivada segunda do deslocamento -> aceleração
+DerivadaSegunda = Derivative(funcao(t), t, 2).doit()
+
+# Mostrando a equação da aceleração
+print("Equação da aceleração: " + str(DerivadaSegunda))
+
+# Calculando a aceleração em 2s
+Aceleracao = Derivative(funcao(t), t, 2).doit().subs({t:2})
+print("Aceleração em 2s: " + str(Aceleracao))
+
+
+
