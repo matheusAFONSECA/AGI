bdfd90dc0891bed0510c889c2ecd077b0b02403d
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-06-23 16:09:23 -0300

    Exercicio 3 incompleto

---
+++ Exercicio3.py
@@ -0,0 +1,17 @@
+# imporatções necessarias para solucionar a questão
+from sympy import Integral, Symbol, exp
+
+# definindo o componente C da expressão
+c = 1994 % 10       # C -> resto do numero de metricula dividido por 10
+
+# definindo o simbolo
+x = Symbol('x')
+
+# definindo a expressão de força variável que esta sendo aplicada no objeto
+def funcao(x):
+    return ((x**2)/5) + x**(4/5) + ((c + 3)*x) - 10
+
+
+# calculando a intergral da função de força variável -> trabalho em entre 1m e 12m de deslocamento
+Trabalho = Integral(funcao(x), (x, 1, 12)).doit()
+print("Trabalho realizado ")
