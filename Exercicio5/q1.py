4081ee22d39358309cd1aa5a76acea15026ec399
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-06-23 21:30:34 -0300

    Exercicio 5 q1 OK

---
+++ Exercicio5/q1.py
@@ -0,0 +1,20 @@
+# imporatções necessarias para solucionar a questão
+from sympy import Symbol, exp, solve
+
+# definindo o componente C da expressão
+c = 1994 % 10       # C -> resto do numero de metricula dividido por 10
+
+# definindo o simbolo
+x = Symbol('x')
+
+
+# definindo a expressão que vamos resolver -> exp: exponencial de base 'e'
+def funcao(x):
+    return exp(-x - 1) + exp(-x - 3) + exp(x) + 5*(c + 1)
+
+
+# solucionando a questão
+resultado = solve(funcao(x))
+
+# mostrando o resultado da questão
+print("A solução da questão é: " + str(resultado))
